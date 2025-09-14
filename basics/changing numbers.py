d73fd9545802bcd14387e8385f98d5c6a4e8ea9f
Aymen Labidi <nightdreamingrhythm@outlook.com>
2025-09-14 11:14:16 -0500

    Add files via upload

---
+++ basics/changing numbers.py
@@ -0,0 +1,14 @@
+# A quilt is 8 squares wide and 12 squares long
+
+quilt_width = 8 
+quilt_length = 12  
+
+# The number of squares needed to create the quilt
+
+print ( quilt_width * quilt_length )
+
+# Scenario where quilt is only 8 squares long
+
+quilt_length = 8
+
+print( quilt_width * quilt_length )
