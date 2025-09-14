400e49f2e83ee6ddf5fc0b14915eb3d3c83143fe
Aymen Labidi <nightdreamingrhythm@outlook.com>
2025-09-14 05:40:16 -0500

    Add files via upload

---
+++ basics/numbers.py
@@ -0,0 +1,17 @@
+# Define the release and runtime integer variables below:
+
+release_year = " 2025 " 
+runtime = " 120 "
+
+print ( ' Release Year: ' )
+print ( release_year )
+
+print ( ' Runtime: ' )
+print ( runtime )
+
+# Define the rating_out_of_10 float variable below: 
+
+rating_out_of_10 = " 9.7 "
+
+print ( ' Rating Out of 10: ' )
+print ( rating_out_of_10 ) 
