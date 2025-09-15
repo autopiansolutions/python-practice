a9f9517addaf4bdd3daab47890f82522e696a3fb
Aymen Labidi <nightdreamingrhythm@outlook.com>
2025-09-15 18:11:59 -0500

    Create block letters.py

---
+++ projects/block letters.py
@@ -0,0 +1,11 @@
+# My name is Aymen Labidi.
+# I love riding around on electric bikes.
+
+
+print ("    A        L")       
+print (" A    A      L")
+print ("A       A    L")
+print ("A A A A A    L")
+print ("A       A    L")
+print ("A       A    L")
+print ("A       A    L L L L L ")
