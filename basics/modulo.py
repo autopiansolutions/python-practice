dcc6ad358e42f4e5d773df932105bade992ab7e1
Aymen Labidi <nightdreamingrhythm@outlook.com>
2025-09-14 19:45:01 -0500

    Add files via upload

---
+++ basics/modulo.py
@@ -0,0 +1,15 @@
+# First order of the day is order number 269
+
+first_order_remainder = 269 % 10
+
+print ( first_order_remainder )
+
+first_order_coupon = "no"
+
+# Second order of the day is order number 270
+
+second_order_remainder = 270 % 10
+
+print ( second_order_remainder )
+
+second_order_coupon = "yes"
